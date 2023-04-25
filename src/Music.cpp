3bf2f148ee3bd25d2f39347cff11791ebc041d97
Khaine <easunxiaoyu2012@163.com>
2023-04-25 18:55:49 +0100

    fix game sound

--- src/Music.cpp
+++ src/Music.cpp
@@ -1,6 +1,3 @@
-//
-// Created by 刘一辰 on 2021/1/11.
-//
 
 #include "Music.h"
 #include <string>
@@ -8,35 +5,29 @@
 
 using namespace irrklang;
 
-ISoundEngine *sound;
+irrklang::ISoundEngine *sound;
 std::string bgmFilename, shootFilename, bloomFilename;
 
-void musicInit(std::string bgmFilename, std::string shootFilename, std::string bloomFilename){
-    #ifdef __APPLE__
+void musicInit(std::string bgmFile, std::string shootFile, std::string bloomFile){
     sound = createIrrKlangDevice();
-    ::bgmFilename = bgmFilename;
-    ::shootFilename = shootFilename;
-    ::bloomFilename = bloomFilename;
-    #endif
+    bgmFilename = bgmFile;
+    shootFilename = shootFile;
+    bloomFilename = bloomFile;
 }
 
 void playBGM(){
-    #ifdef __APPLE__
     sound->setSoundVolume(0.4);
     sound->play2D(bgmFilename.c_str(), true);
-    #endif
 }
 
 void playShootSound(){
-    #ifdef __APPLE__
     sound->setSoundVolume(0.4);
     sound->play2D(shootFilename.c_str(), false);
-    #endif
+    return;
 }
 
 void playBloomSound(){
-    #ifdef __APPLE__
     sound->setSoundVolume(0.8);
     sound->play2D(bloomFilename.c_str(), false);
-    #endif
+    return;
 }
